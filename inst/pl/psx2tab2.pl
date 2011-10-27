ad1e43d2b2d1710a624979503cea179f0f6a43da
f.breitwieser <f.breitwieser@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-10-27 16:27:52 +0000

    allow piping into psx2tab
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/isobar@59736 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/pl/psx2tab2.pl
+++ inst/pl/psx2tab2.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # Creation date : 2011-03-28
-# Last modified : Mon 20 Jun 2011 02:04:40 PM CEST
+# Last modified : Wed 05 Oct 2011 04:10:26 PM CEST
 
 # Module        : psx2tab2.pl
 # Purpose       : 
@@ -12,7 +12,7 @@ use strict;
 use warnings;
 use DBI;
 
-my $psxFile = shift;
+my $psxFile = shift @ARGV;
 my ($protID, $peptide, $modif, $charge, $theo_mass, $exp_mass, 
     $parent_intens, $start_pos, $rtime, @search_engine, @score,$spectrum);
 
@@ -21,8 +21,15 @@ my $numb = "[0-9]+\.?[0-9]*(?:e[+-][0-9]*)?";
 print "accession\tpeptide\tmodif\tcharge\ttheo.mass\texp.mass\tparent.intens".
       "\tstart.pos\tretention.time\tsearch.engine\tscore\tspectrum\n";
 eval{
-  open(XML, $psxFile);
-  while (<XML>){
+  my $XML;
+  my $is_stdin = 0;
+  if (defined $psxFile){
+    open $XML, "<", $psxFile or die $!;
+  } else {
+    $XML = *STDIN;
+    $is_stdin++;
+  }
+  while (<$XML>){
     if (/<idi:proteinId>(.*)<\/idi:proteinId>/)    { $protID = $1; 
     } elsif (/<idi:sequence>(.*)<\/idi:sequence>/) { $peptide = $1;
     } elsif (/<idi:theoMass>(.*)<\/idi:theoMass>/) { $theo_mass = $1; 
@@ -59,7 +66,7 @@ eval{
 
     }
   }
-  close(XML);
+  close($XML) unless $is_stdin;
 };
 if ($@){
   die("Encountered a problem: $@");
