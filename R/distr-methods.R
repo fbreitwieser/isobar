e65b55e31363a7232510984323f42a45465989a9
f.breitwieser <f.breitwieser@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-11-30 13:01:51 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 5 commits.
    Commit information:
    
    Commit id: 47251cc1aca1deb7d22dec88f24a092c62d3259f
    
        Updated vignette
    
    Committed by: Florian Breitwieser
    Author Name: Florian Breitwieser
    Commit date: 2014-11-30 14:00:58 +0100
    Author date: 2014-11-30 14:00:58 +0100
    
    Commit id: cd58b6f5dc76972293f33cfe0a113c19e85bb064
    
        Added calcProbXGreaterThanY.orig for control over integration
    
    Committed by: Florian Breitwieser
    Author Name: Florian Breitwieser
    Commit date: 2014-11-30 13:59:49 +0100
    Author date: 2014-11-30 13:59:49 +0100
    
    Commit id: 2902b5f76da98d745f7fd89fab3b041a0ff7ad29
    
        fix calcProbXDiffNormals() for very small P-values
    
        Instead of returning 1-P(X<=Y), when P(X<=Y) is close or equals to 1,
        calculate P(Y<=X) directly. That fixes potential cases, when the
        resulting P(Y<=X) would be 0.0 due to the loss of precision.
        
    Committed by: Alexey Stukalov
    Author Name: Alexey Stukalov
    Commit date: 2014-11-19 20:37:50 +0100
    Author date: 2014-11-19 08:46:47 +0100
    
    Commit id: aa601958ece4bbe41b8d56d44e270a42ab3244ce
    
        use integrate() for calcProbXGreaterThanY()
    
        use R's built-in integrate() to evaluate P(X>=Y)
        
    Committed by: Alexey Stukalov
    Author Name: Alexey Stukalov
    Commit date: 2014-11-19 20:37:50 +0100
    Author date: 2014-11-19 06:41:05 +0100
    
    Commit id: e29956afabed3e39bfa297e1f13543c7b7b3926f
    
        move plyr package to Imports
    
        to avoid plyr interfering with dplyr in the user session
        
    Committed by: Alexey Stukalov
    Author Name: Alexey Stukalov
    Commit date: 2014-11-19 20:37:50 +0100
    Author date: 2014-11-19 06:25:41 +0100
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/isobar@97232 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/distr-methods.R
+++ R/distr-methods.R
@@ -1,10 +1,14 @@
 
 
 
-calcProbXDiffNormals <- function(X,mu_Y,sd_Y,...,alternative="greater",progress=FALSE) {
-  # calculates an estimate of min(P(X>Y),P(Y>X))
-  #  Y ~ N(mu_Y,sd_Y)
+calcProbXDiffNormals <- function(X,mu_Y,sd_Y,...,alternative=c("greater","less","two-sided"),progress=FALSE) {
+  # calculates a vector of either P(X>=Y) or P(Y>=X)
+  # (depending on 'alternative' option),
+  # where Y ~ N(mu_Y,sd_Y)
+  alternative <- match.arg(alternative)
+
   require(distr)
+  if (!is(X,"Distribution")) stop("X has to be of class Distribution")
 
   if (length(mu_Y) != length(sd_Y)) 
     stop("mu_Y and sd_Y should have equal length")
@@ -12,32 +16,54 @@ calcProbXDiffNormals <- function(X,mu_Y,sd_Y,...,alternative="greater",progress=
   if (isTRUE(progress))
     pb <- txtProgressBar(min=1,max=length(mu_Y))
 
+  X_mode <- distr::q(X)(0.5)
   p.values <- sapply(seq_along(mu_Y),function(i) {
     if (isTRUE(progress))
       setTxtProgressBar(pb,i)
-    if(is.na(mu_Y[i]) || is.na(sd_Y[i])) return(NA)
-    calcProbXGreaterThanY(X,Norm(mu_Y[i],sd_Y[i]),...)
+    if (is.na(mu_Y[i]) || is.na(sd_Y[i])) {
+      return(NA)
+    }
+    else {
+      Y <- Norm(mu_Y[i],sd_Y[i])
+      # FIXME P-value not adjusted for two-sided case
+      if ( alternative == 'greater'
+        || (alternative == 'two-sided' && X_mode < mu_Y[i])
+      ){
+        calcProbXGreaterThanY(X,Y,...)
+      } else {
+        calcProbXGreaterThanY(Y,X,...)
+      }
+    }
   })
   if (isTRUE(progress))
     close(pb)
 
-  switch(alternative,
-         greater=p.values,
-         less=1-p.values,
-         "two-sided"=ifelse(p.values>0.5,1-p.values,p.values),
-         stop("don't know alternative ",alternative))
+  p.values
 }
 
-calcProbXGreaterThanY <- function(X,Y,min.q=10^-6,n.steps=1000) {
-  # calculates an estimate of P(X>Y) + 0.5 P(X=Y)
-  #  a value of 0.5 represents complete overlap
+calcProbXGreaterThanY <- function(X, Y, rel.tol=.Machine$double.eps^0.25, subdivisions=100L)
+{
+  # numerically calculates P(X>=Y)
+  require(distr)
+  if (!is(X,"Distribution")) stop("X has to be of class Distribution")
+  if (!is(Y,"Distribution")) stop("Y has to be of class Distribution")
+#  return ( distr::p(Y-X)(0) ) # the easiest way, but it does not seem to give accurate results for Norm+Tlsd
+
+  # FIXME use the distribution info to deduce integration boundaries
+  return ( integrate( function(t) distr::d(X)(t)*distr::p(Y)(t),
+                      lower=-Inf, upper=Inf, subdivisions = subdivisions,
+                      rel.tol = rel.tol )$value )
+}
+
+calcProbXGreaterThanY.orig <- function(X,Y,min.q=10^-6, subdivisions=100L) {
+  # numerically calculates P(X>=Y)
   require(distr)
   if (!is(X,"Distribution")) stop("X has to be of class Distribution")
   if (!is(Y,"Distribution")) stop("Y has to be of class Distribution")
 
-  steps.seq <- seq(from=min.q,to=1-min.q,length.out=n.steps)
+  steps.seq <- seq(from=min.q,to=1-min.q,length.out=subdivisions)
   steps <- sort(unique(c(q(X)(steps.seq),q(Y)(steps.seq))))
-  nn.steps <- length(steps)
+  nsubdivisions <- length(steps)
            
   dens.X <- d(X)(steps)
   dens.Y <- d(Y)(steps)
@@ -46,7 +72,7 @@ calcProbXGreaterThanY <- function(X,Y,min.q=10^-6,n.steps=1000) {
   dens.total <- sum(dens.X)*sum(dens.Y)
   if (isTRUE(all.equal(dens.total,0))) return(0)
 
-  for (i in 1:nn.steps) {
+  for (i in 1:nsubdivisions) {
     dens.X <- dens.X[-1]
     dens.Y <- dens.Y[-length(dens.Y)]
     dens.sum <- dens.sum + sum(dens.X*dens.Y)
@@ -54,6 +80,7 @@ calcProbXGreaterThanY <- function(X,Y,min.q=10^-6,n.steps=1000) {
 
   dens.sum/dens.total
 }
+
 
 
 distrprint <- function(X,round.digits=5) {
@@ -73,7 +100,7 @@ twodistr.plot <- function(X,Y,n.steps=1000,min.q=10^-3) {
   ggplot(rbind(data.frame(x=steps,Distribution=paste0("X ~ ",distrprint(X)),density=d(X)(steps)),
                data.frame(x=steps,Distribution=paste0("Y ~ ",distrprint(Y)),density=d(Y)(steps)))) + 
     geom_line(aes_string(x="x",y="density",color="Distribution")) +
-    ggtitle(paste0(".5 P(X=Y) + P(X>Y) = ",round(calcProbXGreaterThanY(X,Y,n.steps=n.steps),7)))
+    ggtitle(paste0("P(X>=Y) = ",round(calcProbXGreaterThanY(X,Y),7)))
  
 }
 
